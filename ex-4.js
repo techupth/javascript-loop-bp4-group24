ff51d38e22fe5ba82a3bb25fb3eb6579142e5506
zvryn <sarinkanok@gmail.com>
2024-05-29 09:59:51 +0700

    feat: add completed assignment loops

--- ex-4.js
+++ ex-4.js
@@ -1,6 +1,4 @@
-// Exercise #4: Displaying Restaurant Name
-let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
-let newRestaurants = [];
-// Start coding here
-
-console.log(newRestaurants);
+let restaurantNames = ["KFC","Momo Paradise","Nabezo"];
+for (let name of restaurantNames){
+    console.log("Restaurant Name: "+name);
+};
